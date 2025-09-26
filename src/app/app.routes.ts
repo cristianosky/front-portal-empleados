23b9970ecea4fefe9d555f62d6dc177b25980ed2
Cristian Villalba <cristianvillalba1999@gmail.com>
2025-09-25 20:22:26 -0500

    Se agrego el dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,15 @@ import { Routes } from '@angular/router';
 import { LayautComponent } from './core/layaut/layaut.component';
 import { authGuard } from './guard/auth.guard';
 import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './core/dashboard/dashboard.component';
 
 export const routes: Routes = [
-    { path: '', component: LayautComponent, canActivate: [authGuard] },
+    { 
+        path: '', component: LayautComponent, canActivate: [authGuard],
+        children: [
+            { path: '', component: DashboardComponent },
+        ]
+    },
 
     { path: 'login', component: LoginComponent },
 
--- src/app/core/layaut/layaut.component.ts
+++ src/app/core/layaut/layaut.component.ts
@@ -3,11 +3,12 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { RouterOutlet } from '@angular/router';
 
 
 @Component({
   selector: 'app-layaut',
-  imports: [MatSidenavModule,  MatListModule, MatIconModule, MatButtonModule],
+  imports: [MatSidenavModule,  MatListModule, MatIconModule, MatButtonModule, RouterOutlet],
   templateUrl: './layaut.component.html',
   styleUrls: ['./layaut.component.scss']
 })
